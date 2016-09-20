02536fbd79cd6520ef9a47172d5ea592da07f1e2
Timothy Samson <t.samson@nvusd.org>
2016-09-19 23:57:09 -0700

    Done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,49 @@
 var app = angular.module('main', []);
 
-$(".players button").click(function(event){
+var buttons = $(".players button");
+buttons.addClass("context-menu-one");
 
+var numbers = {
+    one : 0,
+    two : 0,
+    three : 0,
+    four : 0,
+    five : 0,
+    six : 0
+};
+
+buttons.dblclick(function (event){
     var button = $(event.target);
-    alert(button.attr("id"));    
+    var id = button.attr("id");
+
+    numbers[id] -= 3;
+
+    button.css("color", "red");
+    button.html(numbers[id]);
+});
+
+buttons.click(function(event){
+    var button = $(event.target);
+    var id = button.attr("id");
+
+    numbers[id]++;
+
+    button.css("color", "red");
+    button.html(numbers[id]);
+
+});
+
+var reset = '<button class="reset">Reset</button>';
+$(".first div, .second div, .third div").append(reset);
+
+$(".reset").click(function(event){
+
+    var reset = $(event.target);
+    var button = reset.prev();
+    var id = button.attr("id");
+
+    numbers[id] = 0;
+
+    button.html(numbers[id]);
+
 });
