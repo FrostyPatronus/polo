87fe4719813b3d9ade9dde4072b590a571028353
Timothy Samson <t.samson@nvusd.org>
2016-09-24 10:03:59 -0700

    Added click functionality

--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
 var app = angular.module('main', []);
 
 app.controller('scoreboard', ['$scope', function($scope){
-    $scope.positions = 
-    [
-    "One",
-    "Two",
-    "Three",
-    "Four",
-    "Five",
-    "Six"
-    ];
 
+    // DEFINES rows TO BE THE INDIVIDUAL ROWS DATA IN THE VIEW
+    // $scope.rows[rowNumber].score
+    $scope.rows = [];
+    var rows = $scope.rows;
+
+    var positions = ["one", "two", "three", "four", "five", "six"];
     $scope.scores = ["goals", "assists", "steals", "blocks"];
+    var scores = $scope.scores;
+    // Populates the rows list
+    $.each(positions, function(i, v){
+        var dict = {};
+        dict.name = v;
+        $.each(scores, function(i, v){
+            dict[v] = 0;
+        });
+        rows.push(dict);
+    });
+
+    $scope.plusOne = function(row, score){
+        rows[row][score] += 1;
+    };
 
 }]);
 
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("button").click(function(event){
         var button = $(this);
         var id = button.attr("id");
@@ -24,10 +35,12 @@ $(document).ready(function(){
         var position = temp[0];
         var score = temp[1];
 
-        
+        button.html(parseInt(button.html()) + 1);
+
+
 
 
     });
 
-});
+});*/
 
