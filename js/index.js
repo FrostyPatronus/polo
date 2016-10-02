2a5e523c8b78084f677043733bcd03d2ed4f39d7
Timothy Samson <t.samson@nvusd.org>
2016-10-01 18:42:51 -0700

    Did some things

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var app = angular.module('main', []);
 
 var evals = ["goals", "assists", "steals", "blocks"];
-var positions = ["One", "Two", "Three", "Four", "Five", "Six"];
+var positions = ["1", "2", "3", "4", "5", "6"];
 
 function initPlayerObj(name) {
     var player = {name: name};
@@ -19,7 +19,6 @@ app.controller('scoreboard', function() {
     rows: Each of the individual rows' data in the left side
     scores: Each of the evaluation points a player will be grade
     plusOne: Takes a row index and score value and increments it
-    players: All the player objects and their grades
     */
 
     /* 
@@ -29,7 +28,7 @@ app.controller('scoreboard', function() {
     */
 
     // DEFINES rows TO BE THE INDIVIDUAL ROWS DATA IN THE VIEW
-    // this.rows[rowNumber].score
+    // this.rows[rowNumber] = playerObj
     this.positions = positions;
 
     this.rows = [];
@@ -40,7 +39,7 @@ app.controller('scoreboard', function() {
 
     // Populates the rows list
     $.each(positions, function(i, v){
-        var dict = initPlayerObj("EMPTY");
+        var dict = initPlayerObj("Heathen");
         rows.push(dict);
     });
 
@@ -48,15 +47,19 @@ app.controller('scoreboard', function() {
         rows[row][score] += 1;
     };
 
-    /* 
-    -----------------------
-    -- RIGHT HALF ---------
-    -----------------------
-    */
 
+});
+
+/* 
+-----------------------
+-- RIGHT HALF ---------
+-----------------------
+*/
+
+app.controller("RosterController", function(){
     // ALL THE PLAYERS AND THEIR SCORES
     this.players = [];
-    this.name = "";
+    this.name = "fddd";
 
     this.addPlayer = function(){
         if (!this.name)
@@ -67,3 +70,11 @@ app.controller('scoreboard', function() {
 
 });
 
+// DIRECTIVES
+
+app.directive("rowLabel", function() {
+    return {
+        restrict: "E",
+        templateUrl: "html/labels.html"
+    };
+});
